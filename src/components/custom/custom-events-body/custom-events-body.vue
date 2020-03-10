2071525ef0996d529599186235fb8f40cf140192
David Ramirez <david.ramirez@roostergrin.com>
2020-03-10 14:03:29 -0700

    request(custom-events-body)#31290: added widget to fun and events.

--- src/components/custom/custom-events-body/custom-events-body.vue
+++ src/components/custom/custom-events-body/custom-events-body.vue
@@ -14,6 +14,11 @@ export default {
       return this.props.patients
     }
   },
+  mounted: () => {
+    let viralsweep = document.createElement('script')
+    viralsweep.setAttribute('src', 'https://app.viralsweep.com/vsa-widget-1f7921-64321.js?sid=64321_558167')
+    document.head.appendChild(viralsweep)
+  },
   components: {
     BlockTitle
   }
