05530cd9a4eb1006b34b6a680bda8f2cacd0b6a4
Loquen Jones <loquen.jones@roostergrin.com>
2021-01-26 06:59:02 -0800

    request(custom-events-body): update the events script. #49728

--- src/components/custom/custom-events-body/custom-events-body.vue
+++ src/components/custom/custom-events-body/custom-events-body.vue
@@ -14,10 +14,9 @@ export default {
       return this.props.patients
     }
   },
-
   mounted: () => {
     let viralsweep = document.createElement('script')
-    viralsweep.setAttribute('src', 'https://app.viralsweep.com/vsa-widget-9a87a9-75091.js?sid=75091_206124')
+    viralsweep.setAttribute('src', 'https://app.viralsweep.com/vsa-widget-d861a3-81244.js?sid=81244_251491')
     document.head.appendChild(viralsweep)
   },
   components: {
